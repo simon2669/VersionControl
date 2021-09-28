5dd7da49d96aa3a489417706308b999983e04e94
simon2669 <feltalalo26@gmail.com>
2021-09-28 17:02:04 +0200

    third commit

--- irf_versc2/irf_versc2/Form1.cs
+++ irf_versc2/irf_versc2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using irf_versc2.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace irf_versc2
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName; // label1
+            label2.Text = Resource1.FirstName; // label2
+            button1.Text = Resource1.Add; // button1
+
+
+            // listbox1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }

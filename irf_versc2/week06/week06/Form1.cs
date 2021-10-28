813031b6187304f432623cd401df1a34b472411b
simon2669 <feltalalo26@gmail.com>
2021-10-28 16:58:50 +0200

    commit

--- irf_versc2/week06/week06/Form1.cs
+++ irf_versc2/week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.MnbServiceReference1;
 
 namespace week06
 {
@@ -15,6 +16,32 @@ namespace week06
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            // A változó deklarációk jobb oldalán a "var" egy dinamikus változó típus.
+            // A "var" változó az első értékadás pillanatában a kapott érték típusát veszi fel, és később nem változtatható.
+            // Jelen példa első sora tehát ekvivalens azzal, ha a "var" helyélre a MNBArfolyamServiceSoapClient-t írjuk.
+            // Ebben a formában azonban olvashatóbb a kód, és változtatás esetén elég egy helyen átírni az osztály típusát.
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            var result = response.GetExchangeRatesResult;
         }
     }
 }

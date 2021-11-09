0180c71a819950d017cbb31c739ea0e1ce27cb2a
simon2669 <feltalalo26@gmail.com>
2021-11-09 20:51:01 +0100

    commmit

---
+++ irf_versc2/week08/week08/Entities/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week08.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            this.AutoSize = false;
+            this.Height = 50;
+            this.Width = 50;
+            this.Paint += Ball_Paint;
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
--- irf_versc2/week08/week08/Form1.cs
+++ irf_versc2/week08/week08/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Entities;
 
 namespace week08
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
         }
     }
 }
